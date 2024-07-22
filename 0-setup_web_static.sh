4691a8882cac3a0bf9b462958c310ade04b8f960
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-22 12:14:15 -0700

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,11 +18,11 @@ ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/server_name _;/a\
+sudo sed -i "/server_name _;/a\
 \    location /hbnb_static/ {\
 \    \    alias /data/web_static/current/;\
 \    \	  try_files \$uri \$uri/ =404;\
-\    }' /etc/nginx/sites-available/default
+\    }" /etc/nginx/sites-available/default
 
 if [ "$(pgrep -c nginx)" -le 0 ]; then
     sudo service nginx start
