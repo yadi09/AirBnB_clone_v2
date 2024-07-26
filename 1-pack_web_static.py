6966b579cc308a519c623c6f8ce8ec1ed484ec6a
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-26 05:45:10 -0700

    t 6

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive
-from the contents of the web_static folder"""
-
-from fabric.api import local
+"""A module for web application deployment with Fabric."""
+import os
 from datetime import datetime
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """Fabric script that generates a .tgz archive"""
-    local("mkdir -p versions")
-    time = datetime.now()
-    file_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        time.year,
-        time.month,
-        time.day,
-        time.hour,
-        time.minute,
-        time.second
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
     )
-
     try:
-        local("tar -czvf {} web_static/".format(file_path))
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
     except Exception:
-        file_path = None
-
-    return file_path
+        output = None
+    return output
