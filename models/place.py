2bfc7026019b7e571ef2eb981c3453c4b10bab30
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-26 07:20:08 -0700

    Task 7

--- models/place.py
+++ models/place.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
+    __tablename__ = "places"
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), nullable=False, ForignKey(cities.id))
     city_id = ""
     user_id = ""
     name = ""
