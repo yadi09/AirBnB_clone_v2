d0f3e929584ca40fb64217673d688fb5f9d3c53d
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-26 18:40:14 -0700

    fixing error

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+""" Amenity Module for HBNB project """
 import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
-    "Amenity"
+    """Represents an amenity data set."""
     __tablename__ = 'amenities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
