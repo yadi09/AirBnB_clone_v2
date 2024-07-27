1a64be61b56ca8175116d27bd250525f25da913a
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-26 19:19:24 -0700

    fixing error

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,6 @@
--- a script that prepares a MySQL server for the project:
-
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'Hbnb_test_pwd';
-
-GRANT ALL ON hbnb_test_db.* TO hbnb_test@localhost;
-
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
