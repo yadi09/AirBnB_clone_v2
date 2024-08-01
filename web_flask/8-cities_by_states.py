f1e00f48bfc21a2511d41d24a4394aa698284df2
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-01 14:37:53 -0700

    Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-
+'''A simple Flask web application.
+'''
 from flask import Flask, render_template
+
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
-""" app flask """
+'''The Flask application instance.'''
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def cities_by_states():
+    '''The cities_by_states page.'''
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    for state in all_states:
+        state.cities.sort(key=lambda x: x.name)
+    ctxt = {
+        'states': all_states
+    }
+    return render_template('8-cities_by_states.html', **ctxt)
+
+
 @app.teardown_appcontext
-def tear_down(exception):
-    """teardown"""
+def flask_teardown(exc):
+    '''The Flask app/request context end event listener.'''
     storage.close()
 
 
-@app.route("/cities_by_states")
-def cityByState():
-    """city by state"""
-    state_by_city = storage.all(State).values()
-    sorted_data = sorted(state_by_city, key=lambda sc: sc.name)
-    return render_template('8-cities_by_states.html', data=sorted_data)
-
-if (__name__ == "__main__"):
-    app.run(host="0.0.0.0", port="5000", debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
