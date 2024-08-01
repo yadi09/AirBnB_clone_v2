4276f961977914347ea794381721817e4f6463d6
Yadamzer <yadamzerterefe09@gmail.com>
2024-08-01 14:44:30 -0700

    Task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-'''A simple Flask web application.
-'''
-from flask import Flask, render_template
+"""script that starts a Flask web application"""
 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-'''The Flask application instance.'''
+"""app flask"""
 app.url_map.strict_slashes = False
 
 
 @app.route('/cities_by_states')
-def cities_by_states():
-    '''The cities_by_states page.'''
+def cityByState():
+    """citybystate"""
     all_states = list(storage.all(State).values())
     all_states.sort(key=lambda x: x.name)
     for state in all_states:
         state.cities.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('8-cities_by_states.html', **ctxt)
+    return render_template('8-cities_by_states.html', states=all_states)
 
 
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def tear_down(exception):
+    """tear down"""
     storage.close()
 
 
